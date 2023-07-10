80e5bfc2ff2d96d2b35fdf2d8ac1249574bd38e7
Future <futureworldzone@gmail.com>
2023-07-10 13:30:29 +0300

    Completed the Lab for the test cases in README.md.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,18 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".") # end_with? is a default Ruby method to check strings
   end
 
   def question?
-
+    self.end_with?("?") # adding the ? char instead of . to check the string
   end
 
   def exclamation?
-
+    self.end_with?("!") # adding the ! char instead of . to check the string
   end
 
   def count_sentences
-
+    self.scan(/[^\.!?]+[\.!?]/).map(&:strip).count # using scan, map and count with regex to split the input string to sentences
   end
 end
